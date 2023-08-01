8220b60c9963c79ae79c1e857628a035f4f3df51
Kiran1810 <kirangill1810@gmail.com>
2023-08-01 12:30:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+
+const app= express();
+
+app.get('/home',(req,res)=>{
+    res.json({message:'ok'})
+})
+
+app.listen(3000,()=>{
+    console.log('started the server')
+})
